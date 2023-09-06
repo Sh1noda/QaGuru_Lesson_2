fffe2db98c9ab6f40bdb8a9c9d2784fd30501c1f
iLya Novikov <novikov.redstar@yandex.ru>
2023-09-06 19:58:09 +0300

    added new method

--- src/main/java/ru/novikov/Main.java
+++ src/main/java/ru/novikov/Main.java
@@ -2,6 +2,9 @@ package ru.novikov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qaruru!");
+        System.out.println(msg());
+    }
+    private static String msg() {
+        return "Hello qa.guru!!!!";
     }
 }
